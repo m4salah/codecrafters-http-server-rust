6a23dfa6590e81ed1f59d3baffd84e563877c1f8
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2024-01-07 01:59:22 +0200

    pass 3nd stage return 404 for path that not /

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 // Uncomment this block to pass the first stage
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -12,9 +15,21 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                let ok_response = "HTTP/1.1 200 OK\r\n\r\n";
-                stream.write(ok_response.as_bytes()).unwrap();
-                println!("accepted new connection");
+                let mut buffer = [0u8; 1024];
+                stream.read(&mut buffer).unwrap();
+                let string_content = String::from_utf8_lossy(&buffer);
+
+                let mut spli = string_content.split(' ');
+                spli.next().unwrap();
+                if spli.next().unwrap() != "/" {
+                    let ok_response = "HTTP/1.1 404 Not Found\r\n\r\n";
+                    stream.write(ok_response.as_bytes()).unwrap();
+                    println!("accepted new connection");
+                } else {
+                    let ok_response = "HTTP/1.1 200 OK\r\n\r\n";
+                    stream.write(ok_response.as_bytes()).unwrap();
+                    println!("accepted new connection");
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
