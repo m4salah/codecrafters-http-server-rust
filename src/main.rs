ca17e85a7f21c7a750577a448b61f2e86f80b9fa
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2024-01-07 16:57:46 +0200

    stage 6 concurrent connection using std thread

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@
 use std::{
     collections::HashMap,
     io::{Read, Write},
-    net::TcpListener,
+    net::{TcpListener, TcpStream},
+    thread,
 };
 
 fn main() {
@@ -13,63 +14,76 @@ fn main() {
     //
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
+    loop {
+        match listener.accept() {
+            Ok((stream, addr)) => {
+                println!("new client with address: {addr} connected");
+                thread::spawn(move || {
+                    handle_connection(stream);
+                });
+            }
+            Err(e) => println!("Couldn't get client: {:?}", e),
+        }
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
     let not_found_response = "HTTP/1.1 404 Not Found\r\n\r\n";
     let ok_response = "HTTP/1.1 200 OK\r\n\r\n";
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                let mut buffer = [0u8; 1024];
-                // reading from stream to the buffer
-                stream.read(&mut buffer).expect("ERROR: reading stream");
-                let string_content = String::from_utf8_lossy(&buffer);
+    let mut buffer = [0u8; 1024];
+    // reading from stream to the buffer
+    stream.read(&mut buffer).expect("ERROR: reading stream");
+    let string_content = String::from_utf8_lossy(&buffer);
 
-                println!("{}", string_content);
-                let mut spli = string_content.split(' ').skip(1);
+    println!("{}", string_content);
+    let mut spli = string_content.split(' ').skip(1);
 
-                // &str vs String
-                // &str is static size string in stack
-                // String dynamic string in the heap
-                let path = spli.next().expect("Error: getting next");
+    // &str vs String
+    // &str is static size string in stack
+    // String dynamic string in the heap
+    let path = spli.next().expect("Error: getting next");
 
-                if path == "/user-agent" {
-                    let headers: HashMap<&str, &str> = string_content
-                        .lines()
-                        .skip(1)
-                        .filter_map(|line| return line.split_once(": "))
-                        .collect();
-                    let user_agent = headers.get("User-Agent").unwrap();
+    if path == "/user-agent" {
+        let headers: HashMap<&str, &str> = string_content
+            .lines()
+            .skip(1)
+            .filter_map(|line| return line.split_once(": "))
+            .collect();
+        let user_agent = headers.get("User-Agent").unwrap();
 
-                    let response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n", user_agent.as_bytes().len(), user_agent);
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n",
+            user_agent.as_bytes().len(),
+            user_agent
+        );
 
-                    println!("response: {}", response);
-                    stream
-                        .write(response.as_bytes())
-                        .expect("Error: writing to the stream");
-                } else if path.starts_with("/echo/") {
-                    let echo_str = path
-                        .split("/echo/")
-                        .skip(1)
-                        .next()
-                        .expect("ERROR: echo_str");
+        println!("response: {}", response);
+        stream
+            .write(response.as_bytes())
+            .expect("Error: writing to the stream");
+    } else if path.starts_with("/echo/") {
+        let echo_str = path
+            .split("/echo/")
+            .skip(1)
+            .next()
+            .expect("ERROR: echo_str");
 
-                    let response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n", echo_str.as_bytes().len(), echo_str);
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}\r\n",
+            echo_str.as_bytes().len(),
+            echo_str
+        );
 
-                    println!("response: {}", response);
-                    stream
-                        .write(response.as_bytes())
-                        .expect("Error: writing to the stream");
-                } else if path != "/" {
-                    stream.write(not_found_response.as_bytes()).unwrap();
-                    println!("accepted new connection");
-                } else {
-                    stream.write(ok_response.as_bytes()).unwrap();
-                    println!("accepted new connection");
-                }
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        println!("response: {}", response);
+        stream
+            .write(response.as_bytes())
+            .expect("Error: writing to the stream");
+    } else if path != "/" {
+        stream.write(not_found_response.as_bytes()).unwrap();
+        println!("accepted new connection");
+    } else {
+        stream.write(ok_response.as_bytes()).unwrap();
+        println!("accepted new connection");
     }
 }
